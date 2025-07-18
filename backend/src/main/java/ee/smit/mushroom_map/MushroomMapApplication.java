86e563659f92223cee1be8b1141d4c693af92c44
Va1km2e <vaikmae10@gmail.com>
2025-07-18 22:54:08 +0300

    First commit

---
+++ backend/src/main/java/ee/smit/mushroom_map/MushroomMapApplication.java
@@ -0,0 +1,13 @@
+package ee.smit.mushroom_map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MushroomMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MushroomMapApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ee/smit/mushroom_map/MushroomMapApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.smit.mushroom_map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MushroomMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
